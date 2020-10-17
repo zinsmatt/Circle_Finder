68bcbb79eb53c91651ebdfcbdec1bca6f3b4948f
zinsmatt <zins.matthieu@gmail.com>
2020-10-17 23:41:21 +0200

    clean MaskingExample

--- MaskingExample.py
+++ MaskingExample.py
@@ -12,18 +12,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import json
-# For this exercise, the files are collocated with the notebook. 
 
 
 
 
-folders = glob.glob("/media/mzins/DATA1/Topcoder/circle_finder/train/*")
-
-inputs = []
-for f in folders:
-    img_file = os.path.join(f, os.path.basename(f) + "_PAN.tif")
-    annot_file = os.path.join(f, os.path.basename(f) + "_anno.geojson")
-    inputs.append((img_file, annot_file))
+def create_if_not(folder):
+    if not os.path.isdir(folder):
+        os.mkdir(folder)
 
 
 
@@ -34,67 +29,86 @@ def clean_poly(pts):
             out.append(pts[i, :])
     return np.vstack(out)
 
+mode = "test"
+
+print("Mode: ", mode)
+
+dataset_folder = "/media/DATA1/Topcoder/circle_finder/"
+
+if mode == "train" or mode == "valid":
+    folders = glob.glob(os.path.join(dataset_folder, "train/*"))
+else:
+    folders = glob.glob(os.path.join(dataset_folder, "test/*"))
+
+
+inputs = []
+for f in folders:
+    img_file = os.path.join(f, os.path.basename(f) + "_PAN.tif")
+    if mode == "train" or mode == "valid":
+        annot_file = os.path.join(f, os.path.basename(f) + "_anno.geojson")
+    else:
+        annot_file = ""
+    inputs.append((img_file, annot_file))
+
+
+
 labelled_data = []
-idx = 45
-for idx in range(2, len(inputs), 3): #
-# for idx in sorted(list(range(0, len(inputs), 3)) + list(range(1, len(inputs), 3))):
+if mode == "train":
+    list_indices = sorted(list(range(0, len(inputs), 3)) + list(range(1, len(inputs), 3)))
+elif mode == "valid":
+    list_indices = range(2, len(inputs), 3)
+else:
+    list_indices = range(len(inputs))
+
+out_folder = os.path.join(dataset_folder, "circle/%s/" % mode)
+create_if_not(out_folder)
+
+for idx in list_indices:
     print(idx)
+
+    filename = os.path.join(out_folder, "img_%06d.png" % idx)
     f_annotation = inputs[idx][1]
     f_pan = inputs[idx][0]
 
-    darknet_lines = []
-
-    with fiona.open(f_annotation, "r") as annotation_collection:
-        annotations = [feature["geometry"] for feature in annotation_collection]
-                        
-    
     with rasterio.open(f_pan) as src:
-        out_image, out_transform = rasterio.mask.mask(src, annotations, all_touched=False, invert=False, crop=False)
-        out_meta = src.meta
-
         img = src.read(1).astype(float)
+
+
+    img = np.dstack([img]*3)
+    pil_img = Image.fromarray(img.astype(np.uint8))
+
+    x_scale = 1
+    y_scale = 1
+    if img.shape[1] > 800:
+        x_scale = 800 / img.shape[1]
+        y_scale = x_scale
+        new_width = int(round(x_scale * img.shape[1]))
+        new_height = int(round(y_scale * img.shape[0]))
+        pil_img = pil_img.resize((new_width, new_height))
+
+    # pil_img.save(filename)
+    w, h = pil_img.size
+
+    annotations = []
+    if mode == "train" or mode == "valid":
+        with fiona.open(f_annotation, "r") as annotation_collection:
+            annotations = [feature["geometry"] for feature in annotation_collection]
+
+        with rasterio.open(f_pan) as src:
+            out_image, out_transform = rasterio.mask.mask(src, annotations, all_touched=False, invert=False, crop=False)
+            out_meta = src.meta
+
         mask = out_image.squeeze()
         mask[mask!=0] = 1
 
-        img = np.dstack([img]*3)
-        # img[:, :, 0] += 100*mask.astype(float)
-        # img = np.clip(img, 0, 255)
-        
-        pil_img = Image.fromarray(img.astype(np.uint8))
-        #filename = "/home/mzins/dev/Circle_Finder/circle/train/img_%06d.png" % idx
-        #filename = "/media/mzins/DATA1/Topcoder/circle_finder/yolov4/train/img_%06d.png" % idx
-        # filename = "/media/mzins/DATA1/Topcoder/circle_finder/yolov4/valid/img_%06d.png" % idx
-        # filename = "/media/mzins/DATA1/Topcoder/circle_finder/detectron2/train/img_%06d.png" % idx
-        filename = "/media/mzins/DATA1/Topcoder/circle_finder/detectron2/valid/img_%06d.png" % idx
-        # filename = "/home/mzins/dev/darknet/build/darknet/x64/data/obj/img_%06d.png" % idx
 
-        x_scale = 1
-        y_scale = 1
-        if img.shape[1] > 800:
-            x_scale = 800 / img.shape[1]
-            y_scale = x_scale
-            new_width = int(round(x_scale * img.shape[1]))
-            new_height = int(round(y_scale * img.shape[0]))
-            pil_img = pil_img.resize((new_width, new_height))
-        
-        # pil_img.resize((416, 416)).save(filename)
-        pil_img.save(filename)
-        w, h = pil_img.size
-        #plt.imshow(img/255)
-        #plt.show()
-
-
-        # poly = rasterio.features.geometry_mask(geometries=annotations, out_shape=(src.height, src.width), transform=src.transform, all_touched=False, invert=True)
-        # plt.imshow(poly)
-        # plt.show()
         polys = [np.vstack(a["coordinates"]) for a in annotations]
 
-        
         transform = np.asarray(src.transform).reshape((3, 3))
         t_inv = np.linalg.inv(transform)
         annotations = []
         for pts in polys:
-            
+
             pts= pts[:, :2]
             pts = (t_inv @ np.vstack((pts.T, np.ones((1, pts.shape[0]))))).T
             uvs = np.round(pts[:, :2])
@@ -102,9 +116,9 @@ for idx in range(2, len(inputs), 3): #
             uvs[:, 1] *= y_scale
             xmin, ymin = np.min(uvs, axis=0)
             xmax, ymax = np.max(uvs, axis=0)
-            
+
             uvs = clean_poly(uvs)
-            
+
             annot = {}
             annot["bbox"] = [int(xmin), int(ymin), int(xmax), int(ymax)]
             annot["bbox_mode"] = 0
@@ -113,38 +127,16 @@ for idx in range(2, len(inputs), 3): #
             annotations += [annot]
 
 
-            darknet_lines.append("0 %f %f %f %f\n" % (xmin / 416, ymin / 416, (xmax-xmin) / 416, (ymax-ymin) / 416))
-
-
     data = {"file_name": filename,
             "height": h,
             "width": w,
             "id": idx,
             "annotations":annotations}
-    
 
     labelled_data.append(data)
 
-    with open(os.path.splitext(filename)[0] + ".txt", "w") as fout:
-        fout.writelines(darknet_lines)
 
 
-# with open("labels_train.json", "w") as fout:
-with open("labels_valid.json", "w") as fout:
+
+with open(os.path.join(dataset_folder, "circle", "labels_%s.json" % mode), "w") as fout:
     json.dump(labelled_data, fout)
-# out_meta.update({"driver": "GTiff",
-#                  "height": out_image.shape[1],
-#                  "width": out_image.shape[2],
-#                  "transform": out_transform})
-
-
-
-# with rasterio.open("Masked.tif", "w", **out_meta) as dest:
-#     dest.write(out_image)
-
-
-# In[ ]:
-
-
-
-
